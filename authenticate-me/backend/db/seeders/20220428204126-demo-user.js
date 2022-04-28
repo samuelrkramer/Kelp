534f53e30ddfaac1f61d3b126f76c0100c9c6b17
Sam Kramer <SamuelRKramer@gmail.com>
2022-04-28 16:50:16 -0400

    generated and ran Users seeder file';

---
+++ authenticate-me/backend/db/seeders/20220428204126-demo-user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        username: 'JohnDoe',
+        email: 'john@doe.demo',
+        hashedPassword: '$2a$10$gACPTD5ByaJEhG.TyFZyc.I4NtAIujVEHxJ0G2jaxx40E2DOQpPbO'
+      },
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   const Op = Sequelize.Op;
+   return queryInterface.bulkDelete('Users', {
+     username: { [Op.in]: ['JohnDoe', 'Demo-lition', 'FakeUser1', 'FakeUser2']}
+   });
+  }
+};
