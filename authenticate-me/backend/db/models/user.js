371a624c7fc596ec57a32ce3e0ef26eb25d2813e
Sam Kramer <SamuelRKramer@gmail.com>
2022-04-29 13:57:41 -0400

    found another typo in User DB model

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         len: [4, 30],
         isNotEmail(value) {
-          if (ValidatorsImpl.isEmail(value)) {
+          if (Validator.isEmail(value)) {
             throw new Error('Cannot be an email.');
           }
         }
