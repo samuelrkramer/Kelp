6ba2295a62e3259e5d2a0a600ee687839fe168ea
Sam Kramer <SamuelRKramer@gmail.com>
2022-05-03 16:09:43 -0400

    seeder for reviews

---
+++ backend/db/seeders/20220503200808-demo-review.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Reviews', [{
+     userId: 2,
+     businessId: 1,
+     rating: 3,
+     answer: "Tastes kinda fishy to me"
+   }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Reviews', null, {});
+  }
+};
