346ddb9404217c3b687f6a0629b16ed7027523cd
Sam Kramer <SamuelRKramer@gmail.com>
2022-05-05 20:10:34 -0400

    reviews: action creators and thunks done. todo: reducer, and react components

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,142 @@
+import { csrfFetch } from './csrf';
+
+const LOAD_REVIEWS = 'reviews/LOAD_REVIEWS';
+// const LOAD_ONE_BUSINESS = 'business/LOAD_ONE_BUSINESS'
+const ADD_REVIEW = 'reviews/ADD_REVIEW';
+// const UPDATE_BUSINESS = 'business/UPDATE_BUSINESS';
+const DELETE_REVIEW = 'reviews/DELETE_REVIEW';
+
+const loadReviews = (reviews, businessId) => ({
+  type: LOAD_REVIEWS,
+  payload: { reviews, businessId },
+});
+
+// const loadOneBusiness = business => ({
+//   type: LOAD_ONE_BUSINESS,
+//   payload: business,
+// });
+
+const addReview = (review) => {
+  return {
+    type: ADD_REVIEW,
+    payload: review,
+  };
+};
+
+// const updateBusiness = (id, data) => {
+//   return {
+//     type: UPDATE_BUSINESS,
+//     payload: {id, data},
+//   };
+// };
+
+const removeReview = (reviewId, businessId) => {
+  return {
+    type: DELETE_REVIEW,
+    payload: { reviewId, businessId },
+  };
+};
+
+
+export const getBizReviews = (businessId) => async dispatch => {
+  const response = await csrfFetch(`/api/business/${businessId}/reviews`);
+
+  if (response.ok) {
+    const reviews = await response.json();
+    dispatch(loadReviews(reviews));
+  }
+}
+
+// export const fetchOneBusiness = (id) => async dispatch => {
+//   // console.log("fetchOneBusiness thunk fired");
+//   const response = await csrfFetch(`/api/business/${id}`)
+  
+//   if (response.ok) {
+//     const business = await response.json();
+//     // console.log("business:", business);
+//     dispatch(loadOneBusiness(business));
+//   }
+// }
+
+export const createReview = (review, businessId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/business/${businessId}/reviews`, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(review),
+  });
+  const data = await response.json();
+  dispatch(addReview(data));
+  return data;
+};
+
+// export const editBusiness = (business, id) => async (dispatch) => {
+//   const response = await csrfFetch(`/api/business/${id}`, {
+//     method: "PUT",
+//     headers: {"Content-Type": "application/json"},
+//     body: JSON.stringify(business),
+//   });
+//   const data = await response.json();
+//   dispatch(updateBusiness(id, data));
+//   return data
+// };
+
+export const deleteReview = (id) => async (dispatch) => {
+  const response = await csrfFetch(`/api/reviews/${id}`, {
+    method: "DELETE"
+  });
+  if (response.ok) {
+    const delId = await response.json();
+    dispatch(removeReview(delId));
+  }
+}
+
+const initialState = {
+  // list: [],  // possibly more advanced than this project
+};
+
+const businessReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case LOAD_BUSINESSES:
+      const businesses = {};
+      action.payload.forEach(el => {
+        businesses[el.id] = el;
+      });
+      // console.log("from LOAD_BUSINESSES in reducer, businesses:", businesses)
+      return {
+        ...businesses,
+        ...state,
+        // list: action.list
+      };
+    case LOAD_ONE_BUSINESS: {
+      const newState = {...state};
+      // console.log("action:", action)
+      newState[action.payload.id] = action.payload;
+      return newState;
+    }
+    case ADD_BUSINESS: {
+      const newState = {
+        ...state,
+        [action.payload.id]: action.payload
+      };
+      // const businesses = newState.list.map(id => newState[id]);
+      // businesses.push(action.business);
+      return newState;
+    }
+    case UPDATE_BUSINESS: {
+      const newState = {
+        ...state,
+        [action.payload.id]: action.payload.data
+      }
+    }
+    case DELETE_BUSINESS: {
+      const newState = {...state};
+      delete newState[action.payload]
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default businessReducer;
